648b33ed70557867858c73ad0a6673c441685e53
118k <fluorite118@gmail.com>
2019-05-07 22:25:44 +0900

    init: python examples

---
+++ developers-java-example/src/main/java/sdk/functional/WordCount.java
@@ -0,0 +1,19 @@
+package sdk.functional;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class WordCount {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Car Bear Jar");
+		list.add("Lion Jar Bar");
+		list.add("Lion Bar Har");
+		
+		
+
+
+	}
+}
