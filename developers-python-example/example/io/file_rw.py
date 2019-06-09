0f6d2674e12e3d918ed0d02396ddb0c00391087b
118k <fluorite118@gmail.com>
2019-06-09 21:04:48 +0900

    modify: io file

--- developers-python-example/example/io/file_rw.py
+++ developers-python-example/example/io/file_rw.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+# mode
+#    r: 읽기 모드 
+#    w: 쓰기 모드 
+#    a: 추가 모드. 파일의 마지막에 내용 추가 
 with open('파일명', 'r') as f:
     for line in f:
         print(line)
+
+# 추가 모드 
+with open('파일명', 'a') as f:
+    for i in range(1, 10):
+        f.write(i)
+
